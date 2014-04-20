fbcc84bba39f2bcc7e545ade83cf95cdb87f83fb
Erica Lauer Vose <elauervose@gmail.com>
2014-04-20 09:40:30 -0700

    add middleman portfolio files

---
+++ source/twitter/config.php
@@ -0,0 +1,12 @@
+<?php
+
+ $TConfig = array(
+	'access_token' => "X",
+	'access_token_secret'  => "X",
+	'consumer_key'  => "X",
+	'consumer_secret' => "X",
+
+	'screen_name'       => 'webnus',
+	'count'             => 1
+);
+?>
