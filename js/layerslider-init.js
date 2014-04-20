aa098aa9b44dfc85b70a2cbd93ce2f887ac3a8ab
Erica Lauer Vose <elauervose@gmail.com>
2014-04-20 10:18:25 -0700

    Site updated to 7ba328c

---
+++ js/layerslider-init.js
@@ -0,0 +1,20 @@
+/*********************/
+/*
+/*		LayerSlider Initialize
+/*
+/*********************/
+
+
+jQuery(document).ready(function(e) {
+    $('#layerslider').layerSlider({
+		skinsPath : 'layerslider/skins/',
+		skin : 'fullwidth',
+		thumbnailNavigation : 'hover',
+		hoverPrevNext : true,
+		responsive : true,
+		responsiveUnder : 1160,
+		thumbnailNavigation : false,
+		sublayerContainer : 1160
+	});
+});
+
