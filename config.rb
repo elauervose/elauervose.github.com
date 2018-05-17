f555c69975b86bb9eb6a701732e9db968d9f57f4
Erica Lauer Vose <elauervose@gmail.com>
2018-05-16 20:23:24 -0700

    add config rb stuff

--- config.rb
+++ config.rb
@@ -5,6 +5,12 @@ activate :autoprefixer do |prefix|
   prefix.browsers = "last 2 versions"
 end
 
+# Use “pretty” URLs (without the `.html` suffix)
+activate :directory_indexes
+
+# Append hashes to compiled assets
+activate :asset_hash
+
 # Layouts
 # https://middlemanapp.com/basics/layouts/
 
@@ -54,3 +60,4 @@ helpers do
     end
   end
 end
+
